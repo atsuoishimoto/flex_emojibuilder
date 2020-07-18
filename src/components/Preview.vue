5ae4624f811e1a9bcf1bec5c7e54da6b5e1c9d5d
Atsuo Ishimoto <atsuoishimoto@gmail.com>
2020-07-18 09:43:58 +0900

    support google font

--- src/components/Preview.vue
+++ src/components/Preview.vue
@@ -87,14 +87,94 @@
 </template>
 
 <script>
+function GFontToDataURI(url) {
+  return fetch(url) // first fecth the embed stylesheet page
+    .then((resp) => resp.text()) // we only need the text of it
+    .then((text) => {
+      // now we need to parse the CSSruleSets contained
+      // but chrome doesn't support styleSheets in DOMParsed docs...
+      let s = document.createElement("style");
+      s.innerHTML = text;
+      document.head.appendChild(s);
+      let styleSheet = s.sheet;
+
+      // this will help us to keep track of the rules and the original urls
+      let FontRule = (rule) => {
+        let src =
+          rule.style.getPropertyValue("src") ||
+          rule.style.cssText.match(/url\(.*?\)/g)[0];
+        if (!src) return null;
+        let url = src.split("url(")[1].split(")")[0];
+        return {
+          rule: rule,
+          src: src,
+          url: url.replace(/"/g, ""),
+        };
+      };
+      let fontRules = [],
+        fontProms = [];
+
+      // iterate through all the cssRules of the embedded doc
+      // Edge doesn't make CSSRuleList enumerable...
+      for (let i = 0; i < styleSheet.cssRules.length; i++) {
+        let r = styleSheet.cssRules[i];
+        let fR = FontRule(r);
+        if (!fR) {
+          continue;
+        }
+        fontRules.push(fR);
+        fontProms.push(
+          fetch(fR.url) // fetch the actual font-file (.woff)
+            .then((resp) => resp.blob())
+            .then((blob) => {
+              return new Promise((resolve) => {
+                // we have to return it as a dataURI
+                //   because for whatever reason,
+                //   browser are afraid of blobURI in <img> too...
+                let f = new FileReader();
+                f.onload = (e) => resolve(f.result);
+                f.readAsDataURL(blob);
+              });
+            })
+            .then((dataURL) => {
+              // now that we have our dataURI version,
+              //  we can replace the original URI with it
+              //  and we return the full rule's cssText
+              return fR.rule.cssText.replace(fR.url, dataURL);
+            })
+        );
+      }
+      document.head.removeChild(s); // clean up
+      return Promise.all(fontProms); // wait for all this has been done
+    });
+}
+
 export default {
   name: "TextDef",
 
   props: ["divstyle", "html"],
   data: function() {
     return {
-      imageurl: null
+      imageurl: null,
+      cssRules: "",
     };
+  },
+  mounted: function() {
+    console.log("mounted");
+    const url =
+      "https://fonts.googleapis.com/css?family=M PLUS Rounded 1c&text='なるほど'";
+    const url2 = "https://fonts.googleapis.com/css?family=Pangolin";
+    GFontToDataURI(url).then((cssRules) => {
+      //      this.cssRules = "<style>" + cssRules.join("\n") + "</style>";
+      //      console.log(cssRules.length, 4444444444444);
+      const svgNS = "http://www.w3.org/2000/svg";
+      const defs = document.createElementNS(svgNS, "defs");
+      const style = document.createElementNS(svgNS, "style");
+      style.innerHTML = cssRules.join("\n");
+      defs.appendChild(style);
+      this.$refs.svg.appendChild(defs);
+      console.log(11111, this.$refs.svg);
+    });
   },
   updated: function() {
     const svg = this.$refs.svg;
@@ -120,8 +200,8 @@ export default {
       const img = this.$refs.img_128;
       ctx.drawImage(img, 0, 0);
       this.imageurl = canvas.toDataURL("image/png", 1);
-    }
-  }
+    },
+  },
 };
 </script>
 
